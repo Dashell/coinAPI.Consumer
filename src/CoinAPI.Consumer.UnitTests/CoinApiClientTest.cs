1bd607f9a5733d4148b674e33e78f9412b796976
Dashell_J <alain.galas@ineat-conseil.fr>
2020-01-28 18:01:33 +0100

    tes(): add unit test for get coin by id

--- src/CoinAPI.Consumer.UnitTests/CoinApiClientTest.cs
+++ src/CoinAPI.Consumer.UnitTests/CoinApiClientTest.cs
@@ -70,5 +70,51 @@ namespace CoinAPI.Consumer.UnitTests
             // assert
             result.Should().BeEquivalentTo(coins);
         }
+
+        [Fact]
+        public void GetCoinAsync_should_throw_AuthenticationFailedException_when_coinEndpoint_throw_refitException_with_401_statuscode()
+        {
+            // arrange
+            string coindId = "BTC";
+            ApiException refitException = GlobalFixture.CreateRefitApiException(HttpStatusCode.Unauthorized, HttpMethod.Get);
+            // mock
+            coinEndpoint.Setup(mock => mock.GetCoins()).Throws(refitException);
+            // act
+            Func<Task> result = async () => { await client.GetCoinAsync(coindId); };
+
+            // assert
+            result.Should().Throw<AuthenticationFailedException>();
+        }
+
+        [Fact]
+        public void GetCoinAsync_should_throw_CoinNotFoundException_when_coin_not_found_by_id()
+        {
+            // arrange
+            string coindId = "UNK";
+            IEnumerable<Coin> coins = GlobalFixture.ValidCoinList();
+            // mock
+            coinEndpoint.Setup(mock => mock.GetCoins()).Returns(Task.FromResult(coins));
+            // act
+            Func<Task> result = async () => { await client.GetCoinAsync(coindId); };
+
+            // assert
+            result.Should().Throw<CoinNotFoundException>();
+        }
+
+        [Fact]
+        public async Task GetCoinAsync_should_return_coin_for_specific_id()
+        {
+            // arrange
+            string coindId = "BTC";
+            IEnumerable<Coin> coins = GlobalFixture.ValidCoinList();
+            // mock
+            coinEndpoint.Setup(mock => mock.GetCoins()).Returns(Task.FromResult(coins));
+            // act
+            Coin result = await client.GetCoinAsync(coindId);
+
+            // assert
+            result.AssetId.Should().Be(coindId);
+            result.Name.Should().Be("Bitcoin");
+        }
     }
 }
